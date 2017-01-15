69a814c26b2412f450756811565b2b83c5d8fb6e
Brandon Him <brh55@drexel.edu>
2017-01-14 16:33:24 -0800

    Add robust log to include files tested

--- gulpfile.js
+++ gulpfile.js
@@ -17,29 +17,35 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 process.env['SCHEMA_PATH'] = './specs';
 
 _gulp2.default.task('validate', function () {
-    var SCHEMA_PATH = getPath();
-    var patterns = ['./' + SCHEMA_PATH + '/**/swagger.{json,yml,yaml}'];
+  var SCHEMA_PATH = getPath();
+  var patterns = ['./' + SCHEMA_PATH + '/**/swagger.{json,yml,yaml}'];
 
-    return (0, _globby2.default)(patterns).then(function (paths) {
-        return Promise.all(paths.map(validate)).then(pass).catch(fail);
-    });
+  return (0, _globby2.default)(patterns).then(function (paths) {
+    return Promise.all(paths.map(validate)).then(pass).catch(fail);
+  });
 });
 
 _gulp2.default.task('default', ['validate']);
 
 function validate(path) {
-    return _swaggerParser2.default.validate(path);
+  return _swaggerParser2.default.validate(path);
 }
 
-function pass() {
-    console.log('Validation passed, schemas are valid');
+function pass(apis) {
+  if (!apis) console.log('No swagger files found');
+
+  var apiTitles = apis.map(function (api) {
+    return api.info.title + ' ' + api.info.version;
+  }).toString().replace(',', ' , ');
+
+  console.log('Swagger API Definition Passed \n\nAPIs Tested: ' + apiTitles);
 }
 
 function fail(err) {
-    throw new Error(err);
+  throw new Error(err);
 }
 
 function getPath() {
-    if (typeof process.env.SCHEMA_PATH !== 'string') throw new Error('env SCHEMA_PATH is not defined.');
-    return process.env.SCHEMA_PATH;
+  if (typeof process.env.SCHEMA_PATH !== 'string') throw new Error('env SCHEMA_PATH is not defined.');
+  return process.env.SCHEMA_PATH;
 }
--- src/gulpfile.js
+++ src/gulpfile.js
@@ -5,32 +5,41 @@ import swagger from 'swagger-parser';
 process.env['SCHEMA_PATH'] = './specs';
 
 gulp.task('validate', () => {
-    var SCHEMA_PATH = getPath();
-    var patterns = [`./${SCHEMA_PATH}/**/swagger.{json,yml,yaml}`];
-    
-    return globby(patterns).then((paths) => {
-        return Promise.all(paths.map(validate))
-            .then(pass)
-            .catch(fail);
-    });
+  var SCHEMA_PATH = getPath();
+  var patterns = [`./${SCHEMA_PATH}/**/swagger.{json,yml,yaml}`];
+  
+  return globby(patterns).then((paths) => {
+    return Promise.all(paths.map(validate))
+      .then(pass)
+      .catch(fail);
+  });
 });
 
 gulp.task('default', ['validate']);
 
 function validate(path) {
-    return swagger.validate(path);
+  return swagger.validate(path);
 }
 
-function pass() {
-    console.log('Validation passed, schemas are valid');
+function pass(apis) {
+  if (!apis)
+    console.log('No swagger files found');
+
+  var apiTitles = apis
+                    .map((api) => {
+                      return `${api.info.title} ${api.info.version}`})
+                    .toString()
+                    .replace(',', ' , ');
+
+  console.log(`Swagger API Definition Passed \n\nAPIs Tested: ${apiTitles}`);
 }
 
 function fail(err) {
-    throw new Error(err);
+  throw new Error(err);
 }
 
 function getPath() {
-    if (typeof process.env.SCHEMA_PATH !== 'string')
-        throw new Error('env SCHEMA_PATH is not defined.');
-    return process.env.SCHEMA_PATH;
+  if (typeof process.env.SCHEMA_PATH !== 'string')
+    throw new Error('env SCHEMA_PATH is not defined.');
+  return process.env.SCHEMA_PATH;
 }
